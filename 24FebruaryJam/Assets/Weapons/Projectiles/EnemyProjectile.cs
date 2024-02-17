a262ae3d94d7eafdd7845505508e8e4233c893cc
OscarWirdelius <oscar.wirdelius@gmail.com>
2024-02-17 23:14:50 +0100

    Shooter tower

---
+++ 24FebruaryJam/Assets/Weapons/Projectiles/EnemyProjectile.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    [HideInInspector] public virtual float Damage { get; set; }
+
+    public Rigidbody RB;
+
+    float lifetime = 10.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerControllerScript playerController))
+        {
+            playerController.TakeDamage(Damage);
+            // Add animation later.
+            ObjectPooler.EnqueuObject(this, "EnemyDefaultProjectile");
+        }
+
+    }
+
+    private void Update()
+    {
+        lifetime -= Time.deltaTime;
+
+        if (lifetime < 0.0f)
+        {
+            ObjectPooler.EnqueuObject(this, "EnemyDefaultProjectile");
+        }
+    }
+}
