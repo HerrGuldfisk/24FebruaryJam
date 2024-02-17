39c205f2805e4d11e3ac5f58331fc2867cd71492
OscarWirdelius <oscar.wirdelius@gmail.com>
2024-02-17 14:01:17 +0100

    aids

--- 24FebruaryJam/Assets/Player/PlayerControllerScript.cs
+++ 24FebruaryJam/Assets/Player/PlayerControllerScript.cs
@@ -7,37 +7,50 @@ using UnityEngine.InputSystem;
 public class PlayerControllerScript : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-
-    public PlayerStats playerStats = new PlayerStats();
+    public PlayerStats playerStats = new();
 
     private Vector2 movementInput = Vector2.zero;
+    private Vector2 lookInput = Vector2.zero;
 
-    public void OnMove(InputValue inputValue)
-    {
+    [SerializeField]
+    private float rotationSmoothing = 100.0f;
+
+    public void OnMove(InputValue inputValue) {
         movementInput = inputValue.Get<Vector2>();
     }
 
-    private void Start()
-    {
+    /*public void OnLook(InputValue inputValue) {
+        lookInput = inputValue.Get<Vector2>();
+    }*/
+
+    private void Start() {
         controller = gameObject.GetComponent<CharacterController>();
     }
 
-    void Update()
-    {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+    void Update() {
+        UpdateMovement();
+        //UpdateRotation();
+    }
 
+    void UpdateMovement() {
         Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
         controller.Move(move * Time.deltaTime * playerStats.MoveSpeed.Value);
 
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
+        }
+    }
+
+    void UpdateRotation() {
+        if (Mathf.Abs(lookInput.x) > 0 || Mathf.Abs(lookInput.y) > 0)
+        {
+            Vector3 playerDir = Vector3.right * lookInput.x + Vector3.forward * lookInput.y;
+            if (playerDir.sqrMagnitude > 0.0f)
+            {
+                Quaternion newrotation = Quaternion.LookRotation(playerDir, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, newrotation, rotationSmoothing * Time.deltaTime);
+            }
         }
     }
 }
