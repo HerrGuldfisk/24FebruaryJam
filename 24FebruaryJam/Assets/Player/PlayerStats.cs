5c71383fd911ed57a186746a06279d75a5abcb3f
Theodor <telo_tennis@hotmail.com>
2024-02-17 12:04:05 +0100

    Weapons

--- 24FebruaryJam/Assets/Player/PlayerStats.cs
+++ 24FebruaryJam/Assets/Player/PlayerStats.cs
@@ -5,6 +5,5 @@ public class PlayerStats : MonoBehaviour
 {
     public CharacterStat MaxHP { get; set; } = new CharacterStat(1);
     public CharacterStat CurrentHP { get; set; } = new CharacterStat(1);
-
-    public CharacterStat BaseSpeed { get; set; } = new CharacterStat(5);
+    public CharacterStat MoveSpeed { get; set; } = new CharacterStat(5);
 }
